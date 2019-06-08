ee1d12c5fbdf8745ab6e42f8db4a479e764c915c
David Mohamad <davidkmohamad@gmail.com>
2019-06-08 20:27:49 +0100

    bump version 0.2.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="linearstage",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     install_requires=['RPi.GPIO'],
 )
