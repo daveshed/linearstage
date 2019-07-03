f9065a0de89f1b8b52037b076facce62851b5848
David Mohamad <davidkmohamad@gmail.com>
2019-07-03 22:09:56 +0100

    stepper motor sequence is more obvious

--- stage/config.py
+++ stage/config.py
@@ -22,26 +22,26 @@ def setup_logger():
         stream=stdout,
         level=logging.INFO)
 
-COIL_A1_PIN = 13 # pink
-COIL_A2_PIN = 26 # orange
-COIL_B1_PIN = 6  # blue
-COIL_B2_PIN = 19 # yellow
+COIL_A1_PIN = 26 # orange
+COIL_B1_PIN = 19 # yellow
+COIL_A2_PIN = 13 # pink
+COIL_B2_PIN = 6  # blue
 
 SEQUENCE = [
+    [1, 0, 0, 0],
+    [1, 1, 0, 0],
     [0, 1, 0, 0],
-    [0, 1, 0, 1],
+    [0, 1, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
     [0, 0, 0, 1],
     [1, 0, 0, 1],
-    [1, 0, 0, 0],
-    [1, 0, 1, 0],
-    [0, 0, 1, 0],
-    [0, 1, 1, 0],
 ]
 
 END_STOP_PIN = 22
 
 MOTOR_CONFIG = {
-    'pins': [COIL_A1_PIN, COIL_A2_PIN, COIL_B1_PIN, COIL_B2_PIN],
+    'pins': [COIL_A1_PIN, COIL_B1_PIN, COIL_A2_PIN, COIL_B2_PIN],
     'sequence': SEQUENCE,
     'ms_delay': 10,
 }
